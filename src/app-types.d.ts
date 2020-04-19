36c0b9eaa373837a1ac85fa4dd304b0529ac9cd7
Janishar Ali <janishar.ali@gmail.com>
2020-04-20 03:28:14 +0530

    define material-ui theme

--- src/app-types.d.ts
+++ src/app-types.d.ts
@@ -15,3 +15,31 @@ declare global {
 		__PRELOADED_STATE__: any;
 	}
 }
+
+declare module '@material-ui/core/styles/createMuiTheme' {
+	interface Theme {
+		custom: {
+			colors: {
+				white: React.CSSProperties['color'];
+				light: React.CSSProperties['color'];
+				black: React.CSSProperties['color'];
+				dark: React.CSSProperties['color'];
+				gray: React.CSSProperties['color'];
+				aliceBlue: React.CSSProperties['color'];
+			};
+		};
+	}
+	// allow configuration using `createMuiTheme`
+	interface ThemeOptions {
+		custom: {
+			colors: {
+				white: React.CSSProperties['color'];
+				light: React.CSSProperties['color'];
+				black: React.CSSProperties['color'];
+				dark: React.CSSProperties['color'];
+				gray: React.CSSProperties['color'];
+				aliceBlue: React.CSSProperties['color'];
+			};
+		};
+	}
+}
