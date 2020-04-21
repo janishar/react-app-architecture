f058787377390f98459216d3445ac55a61365fe1
Janishar Ali <janishar.ali@gmail.com>
2020-04-22 00:02:29 +0530

    add simple component generator

---
+++ .templates/simple_component/index.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react';
+import useStyles from './style';
+
+export default function Component(): ReactElement {
+    const classes = useStyles();
+    return <div className={classes.root}></div>;
+}
